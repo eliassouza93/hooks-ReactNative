c4365824c6a02bae80440a8427f0e0b977a0cf50
Elias de Souza <elias99-souza@hotmail.com>
2023-03-07 20:11:42 -0300

    stack_Navigator

---
+++ src/Produtor/index.js
@@ -0,0 +1,6 @@
+import { Text } from "react-native/types";
+
+
+export default function Produtor() {
+    return <Text>Produtor</Text>
+}
---
+++ src/rotas/ProdutorRotas.js
@@ -0,0 +1,10 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator()
+
+export default function ProdutorRotas() {
+    return
+     <Stack.Navigator>
+         
+    </Stack.Navigator>
+}
